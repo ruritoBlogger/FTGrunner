7eac2b9597e6ab05139fbc77e6c7b12f16109ba7
ruritoblogger <rurito.blog@gmail.com>
2021-06-27 19:11:16 +0900

    refactor: 画像を作成する部分とログから情報を収集する部分を分離

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,4 @@
 from .error import *
 from .config import *
 from .create_picture import create_graph
+from .get_result import get_result
---
+++ util/get_result.py
@@ -0,0 +1,33 @@
+import glob
+import csv
+from typing import List
+
+
+def get_result(dir_path: str) -> List[float]:
+    """試合のログ(csv)から勝敗データを抽出する
+
+    Args:
+        file_path (str): 抽出したい試合のログが格納されているパス
+
+    Returns:
+        List[float]: 勝率データ
+    """
+
+    files = glob.glob(dir_path + "*.csv")
+    result = []
+
+    for file in files:
+        csv_file = open(file, "r")
+        csv_data = csv.reader(csv_file, delimiter=",", doublequote=True,
+                              lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+
+        win = 0
+        lose = 0
+        for data in csv_data:
+            if int(data[1]) >= int(data[2]):
+                win += 1
+            else:
+                lose += 1
+        result.append(win/(win+lose))
+
+    return result
