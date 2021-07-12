86bcebd406aee841ee8c37ccb692fc95062dc5ce
ruritoblogger <rurito.blog@gmail.com>
2021-07-12 15:17:49 +0900

    feat: .envファイルに指定されたキーが存在しない時に投げるエラーを定義

--- util/error.py
+++ util/error.py
@@ -5,3 +5,7 @@ class DirectoryNotFoundError(Exception):
 
 class DirectoryAlreadyExistsError(Exception):
     """指定されたディレクトリが既に存在する時に投げるエラー"""
+
+
+class DotenvKeyNotFoundError(Exception):
+    """.envファイルに指定されたキーが存在しない時に投げるエラー"""
